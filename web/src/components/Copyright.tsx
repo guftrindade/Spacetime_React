24f8501bac23d9d2f381c2cbbf6f712ef8ad1cf6
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-06-08 16:34:26 -0300

    feat: implementado funcionalidades

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="leading relaxed text-sm text-gray-200">
+      Feito durante o NLW por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-50"
+        href="https://www.linkedin.com/in/gustavoftrindade/"
+      >
+        Gustavo Trindade
+      </a>
+    </div>
+  )
+}
